c0ba05e6ae39a94cd88b78de8775d473a2d9818f
Vadym <petrukvadd@gmail.com>
2024-03-19 21:31:47 +0200

    added UserModel

--- authors_api/urls.py
+++ authors_api/urls.py
@@ -1,8 +1,31 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 
-urlpatterns = [
-    path(settings.ADMIN_URL, admin.site.urls),
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 
+from rest_framework import permissions
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Authors API',
+        default_version='v1',
+        description='API endpoints for Authors API',
+        contact=openapi.Contact(email='djangoverific@gmail.com'),
+        license=openapi.License(name='MIT license')
+    ),
+    public = True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
+urlpatterns = [
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0)),
+    path(settings.ADMIN_URL, admin.site.urls),
 ]
+
+admin.site.site_header = 'Authors API Admin'
+admin.site.site_title = 'Authors API Admin Portal'
+admin.site.index_title = 'Welcome to Authors API Portal'
