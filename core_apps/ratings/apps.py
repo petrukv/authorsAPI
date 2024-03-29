26e6a1d239943d557e93b7a008a544c9423eb27b
Vadym <petrukvadd@gmail.com>
2024-03-29 18:25:12 +0200

    added article model and migrations

--- core_apps/ratings/apps.py
+++ core_apps/ratings/apps.py
@@ -3,5 +3,5 @@ from django.utils.translation import gettext_lazy as _
 
 class RatingsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "ratings"
+    name = "core_apps.ratings"
     verbose_name = _('Ratings')
