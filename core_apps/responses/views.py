7e51b26714df1b1d34b67b754e9164c5c5a9823b
Vadym <petrukvadd@gmail.com>
2024-03-30 19:13:35 +0200

    Response views

--- core_apps/responses/views.py
+++ core_apps/responses/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.generics import get_object_or_404
+from rest_framework.exceptions import PermissionDenied
+from rest_framework import permissions
 
-# Create your views here.
+from .models import Article, Response
+from .serializers import ResponseSerializer
+
+
+class ResponseListCreateView(generics.ListCreateAPIView):
+    queryset = Response.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = ResponseSerializer
+
+    def get(self, request, *args, **kwargs):
+        article_id = self.kwargs.get('article_id')
+        return Response.objects.filter(article__id=article_id, parent_response=None)
+    
+    def perform_create(self, serializer):
+        user = self.request.user
+        article_id = self.kwargs.get('article_id')
+        article = get_object_or_404(Article, id=article_id)
+        serializer.save(user=user, article=article)
+
+class ResponseUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Response.object.all()
+    serializer_class = ResponseSerializer
+    lookup_field = 'id'
+
+    def perform_update(self, serializer):
+        user = self.request.user
+        response = self.get_object()
+        if user != response.user:
+            raise PermissionDenied('YOu do not have permission to edit this response ')
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        user = self.request.user
+        response = self.get_object()
+        if user != response.user:
+            raise PermissionDenied('YOu do not have permission to delete this response ')
+        instance.delete()
